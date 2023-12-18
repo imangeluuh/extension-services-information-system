88977ecf2d3997d5f2f0cef511eba25024f23a21
Angeluuh <angelanicolebatoon@gmail.com>
2023-12-19 02:54:32 +0800

    fix: add countdown time to local storage to prevent resetting time on page reload, add to condition to display many attempt message

--- app/static/js/login_attempt.js
+++ app/static/js/login_attempt.js
@@ -21,8 +21,8 @@ function startCountdown() {
         localStorage.setItem('countdownTime', countdownTime);
         if (countdownTime <= 0) {
             clearInterval(countdownInterval); // Stop the countdown when it reaches 0
-            resetEntrySession(); // Reset entry session on the server
             localStorage.setItem('entry', 3); // Reset attempts in localStorage
+            localStorage.setItem('countdownTime', 900); // Reset countdown time in localStorage
             countdownDisplay.textContent = ''; // Clear countdown display
             document.getElementById('manyAttempt').style.display = 'none'; // Hide the attempt message
             disableForm(false); // Enable form fields and submit button after countdown
@@ -38,12 +38,14 @@ function startCountdown() {
 
 // Check entry count on page load and initiate countdown if entry is zero
 document.addEventListener("DOMContentLoaded", function(event) {
-    let entry = localStorage.getItem('entry') || 3; // Get the stored entry count or set to 3 initially
-    let countdownDisplay = document.getElementById('countdownDisplay'); // Reference to the countdown display span
     if (localStorage.getItem('countdownTime') == null) {
         localStorage.setItem('countdownTime', 900);
+    } 
+
+    if (localStorage.getItem('entry') == null) {
+        localStorage.setItem('entry', 3);
     }
-    if (entry <= 0) {
+    if (localStorage.getItem('countdownTime') < 900 || localStorage.getItem('entry') <= 0)  {
         disableForm(true); // Disable form fields and submit button
         document.getElementById('manyAttempt').style.display = 'inline-block'; // Show the attempt message
         startCountdown(); // Start the countdown
